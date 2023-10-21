feb01cf4b8c63461c6db3e81f76d149648c4b900
Muzammil Khokhar <muzammilkhokhar851@gmail.com>
2023-10-21 12:24:49 +0500

    added porper createTender api according to frontend and also added api to view all tenders

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -10,24 +10,22 @@
 // // Importing Routes
 // import Email from './src/routes/EmailVerification.js';
 // import Contractors from './src/routes/Contractor_Routes.js';
-const express = require('express');
-const dotenv = require('dotenv');
-const connect = require('./src/config/DataBaseConnection.js');
-const cors = require('cors');
-const passport = require('passport');
+const express = require("express");
+const dotenv = require("dotenv");
+const connect = require("./src/config/DataBaseConnection.js");
+const cors = require("cors");
+const passport = require("passport");
 
-const { passportLocalSetup } = require('./passport.js');
-const expressSession = require('express-session');
+const { passportLocalSetup } = require("./passport.js");
+const expressSession = require("express-session");
 
 // Importing Routes
-const Email = require('./src/routes/EmailVerification.js');
-const Contractors = require('./src/routes/Contractor_Routes.js');
+const Email = require("./src/routes/EmailVerification.js");
+const Contractors = require("./src/routes/Contractor_Routes.js");
 
 const port = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
-
-
 
 // To parse json data
 app.use(express.json());
@@ -45,12 +43,9 @@ app.use(
   })
 );
 
-
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 
 // To avoid cors error
 app.use(
@@ -61,57 +56,72 @@ app.use(
   })
 );
 
-
 // Web3
 const ABI = require("./ABI.json");
 const { Web3 } = require("web3");
-const web3 = new Web3("https://eth-sepolia.g.alchemy.com/v2/ks2UXGvvtc2BEiJ5RUzKmKgcBUCCldJJ")
-const contractAddress = "0x4d2E0830CC8bfC4BB60dc9dfe6875C7de7cCaE23";
+const web3 = new Web3(
+  "https://eth-sepolia.g.alchemy.com/v2/ks2UXGvvtc2BEiJ5RUzKmKgcBUCCldJJ"
+);
+const contractAddress = "0x88B238271e8B6f0AB102F65f2A767996B1D38C84";
 const contract = new web3.eth.Contract(ABI, contractAddress);
 
 app.post("/createTender", async (req, res) => {
   try {
+    const {
+      name,
+      contractTitle,
+      description,
+      startDate,
+      endDate,
+      tenderNumber,
+    } = req.body;
 
-    // const description = req.body.description;
-    // console.log(description);
-    // console.log("hello");
-    const tender = await contract.methods.createTender("09876543wetyukjhgfdxc").call();
-    //console.log(tender);
-    //const num = tcount;
-    //console.log("count: ",tcount);
+    const tender = await contract.methods
+      .createTender(
+        name,
+        contractTitle,
+        description,
+        startDate,
+        endDate,
+        tenderNumber
+      )
+      .call();
 
-    //const count = Number(num);
-    // const taskObj={
-    //     numId,name,date
-    // }
-    res.status(200).json({ tender, status: 200 })
+    res.status(200).json({ status: 200, message: "Tender Created" });
   } catch (error) {
-    res.status(404).json({ status: 500 })
-    console.error(error)
+    res.status(404).json({ status: 500 });
+    console.error(error);
   }
-})
+});
 
+app.get("/viewAllTenders", async (req, res) => {
+  try {
+    const viewAllTenders = await contract.methods.getAllTenders().call();
+
+    res.status(200).json({ status: 200, viewAllTenders });
+  } catch (error) {
+    res.status(404).json({ status: 500 });
+    console.error(error);
+  }
+});
 
 // Connection to the database
 const database_url = process.env.DATABASE;
 connect(database_url);
 
 // Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-
 // Define a route for the root URL
-app.use('/email', Email);
-app.use('/contractor', Contractors);
+app.use("/email", Email);
+app.use("/contractor", Contractors);
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-
 
 /*
 
